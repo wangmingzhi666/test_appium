c19298af3016a1847351fd12e3f3e942840675c5
wangmingzhi666 <45446077+wangmingzhi666@users.noreply.github.com>
2020-06-28 09:36:42 +0800

    Update and rename base/base_yml.py to 基础/base_yml.py

--- 基础/base_yml.py
+++ 基础/base_yml.py
@@ -2,7 +2,7 @@ import yaml
 
 
 def yml_data_with_file(filename):
-    with open('../data/'+filename+'.yml', 'r')as f:
+    with open('./data/'+filename+'.yml', 'r')as f:
         data = yaml.load(f, Loader=yaml.FullLoader)
 
     return data
