cafe0bfdf7a345c16eb9d1ae38a17fcc28338877
Wang mingzhi <wangmingzhi233@163.com>
2020-06-28 09:04:40 +0800

    newest

--- yml_demo.py
+++ yml_demo.py
@@ -2,7 +2,7 @@ import yaml
 
 def main():
 
-    with open('test.yml', 'r')as f:
+    with open('data.yml', 'r')as f:
         data1 = yaml.load(f, Loader=yaml.FullLoader)
         # yaml.dump(data, f, encoding='utf-8', allow_unicode=True)
         print(type(data1))
